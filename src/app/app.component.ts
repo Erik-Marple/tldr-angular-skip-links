2cdb501337c895845e630a208483d38c1851dd1e
Erik Marple <erik.marple@live.com>
2017-03-28 07:44:45 -0500

    skip link implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, Renderer, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  @ViewChild('main') main: ElementRef;
+
+  constructor(
+    private renderer: Renderer
+  ) {}
+
+  skipLink() {
+    this.renderer.invokeElementMethod(this.main.nativeElement, 'focus');
+  }
+
+  ngOnInit() {
+
+  }
 }
